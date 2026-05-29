592eaa0ed8c45517f85dca3f8b6449e2c2cc6efc
suv <suvindu.20240294@iit.ac.lk>
2026-05-29 20:35:53 +0530

    test(backend): add application context test

---
+++ advertiser_backend/src/test/java/com/example/sampleApp/SampleAppApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.sampleApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAppApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
